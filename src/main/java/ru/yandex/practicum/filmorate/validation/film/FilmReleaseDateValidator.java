683b08712e34c1e3683e29c24b7e624b68aca986
Andrei Borisov <o10i@yandex.ru>
2022-11-29 12:09:48 +0300

    Исправлены замечания.

--- src/main/java/ru/yandex/practicum/filmorate/validation/film/FilmReleaseDateValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validation/film/FilmReleaseDateValidator.java
@@ -5,12 +5,14 @@ import javax.validation.ConstraintValidatorContext;
 import java.time.LocalDate;
 
 public class FilmReleaseDateValidator implements ConstraintValidator<FilmReleaseDateConstraint, LocalDate> {
+    public static final LocalDate DATE = LocalDate.of(1895, 12, 27);
+
     @Override
     public void initialize(FilmReleaseDateConstraint constraintAnnotation) {
     }
 
     @Override
     public boolean isValid(LocalDate releaseDate, ConstraintValidatorContext constraintValidatorContext) {
-        return releaseDate.isAfter(LocalDate.of(1895, 12, 27));
+        return releaseDate.isAfter(DATE);
     }
 }
